55a5d4c8152b39c4c6e78d8b6e3f9c767c80d742
Kevin-Liu-01 <66856750+Kevin-Liu-01@users.noreply.github.com>
2021-08-18 17:24:26 -0400

    my first commit

---
+++ src/components/create.js
@@ -0,0 +1,160 @@
+import React, { useState } from "react";
+// This will require to npm install axios
+import axios from 'axios';
+import { withRouter } from "react-router";
+
+const Create = (props) => {
+  const [stateObj = {}, _objSet] = useState({ person_name: "", person_position: "", person_level: "" })
+  const myStateRef = React.useRef(stateObj);
+
+  const objSet = data => {
+    myStateRef.current = data;
+    _objSet(data);
+  };
+
+
+  console.log("The initial state is " + JSON.stringify(stateObj))
+  // componentWillMount(){
+  //   console.log("Componenet is mounting")
+  // }
+
+  // componentWillUnmount(){
+  //   console.log("Componenet is unommounting")
+  // }
+  // componentDidMount(){
+  //   console.log("Componenet mounted")
+  // }
+  // componentDidUpdate(){
+  //   console.log("Componenet updated")
+  // }
+
+  // These methods will update the state properties.
+  function onChangePersonName(e) {
+    console.log(" prev state: " + JSON.stringify(myStateRef.current));
+    objSet({ ...myStateRef.current, person_name: e.target.value })
+  }
+
+  function onChangePersonPosition(e) {
+    objSet({ ...myStateRef.current, person_position: e.target.value })
+  }
+
+
+  function onChangePersonLevel(e) {
+    objSet({ ...myStateRef.current, person_level: e.target.value })
+  }
+
+  // This function will handle the submission.
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    // When post request is sent to the create url, axios will add a new record(newperson) to the database.
+    const newperson = {
+      person_name: stateObj.person_name,
+      person_position: stateObj.person_position,
+      person_level: stateObj.person_level,
+      
+    };
+
+    axios
+      .post("http://localhost:3000/record/add", newperson)
+      .then((res) => {console.log(res.data);});
+ 
+    
+    // await axios.post("http://localhost:3000/record/add", newperson);
+    console.log("Timer begin")
+
+    
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    props.history.push("/");
+
+    console.log("new person has been crated")
+
+
+    // We will empty the state after posting the data to the database
+    objSet({
+      person_name: "",
+      person_position: "",
+      person_level: "",
+    });
+
+    
+
+  }
+
+
+  // This following section will display the form that takes the input from the user.
+  return (
+    <div style={{ marginTop: 20 }}>
+      <h3>Create New Record</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Name of the person: </label>
+          <input
+            type="text"
+            className="form-control"
+            value={stateObj.person_name}
+            onChange={onChangePersonName}
+          />
+        </div>
+        <div className="form-group">
+          <label>Person's position: </label>
+          <input
+            type="text"
+            className="form-control"
+            value={stateObj.person_position}
+            onChange={onChangePersonPosition}
+          />
+        </div>
+        <div className="form-group">
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="priorityOptions"
+              id="priorityLow"
+              value="Intern"
+              checked={stateObj.person_level === "Intern"}
+              onChange={onChangePersonLevel}
+            />
+            <label className="form-check-label">Intern</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="priorityOptions"
+              id="priorityMedium"
+              value="Junior"
+              checked={stateObj.person_level === "Junior"}
+              onChange={onChangePersonLevel}
+            />
+            <label className="form-check-label">Junior</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="priorityOptions"
+              id="priorityHigh"
+              value="Senior"
+              checked={stateObj.person_level === "Senior"}
+              onChange={onChangePersonLevel}
+            />
+            <label className="form-check-label">Senior</label>
+          </div>
+        </div>
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Create person"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default withRouter(Create);
---
+++ src/components/edit.js
@@ -0,0 +1,153 @@
+import React, { useEffect, useState } from "react";
+// This will require to npm install axios
+import axios from "axios";
+import { withRouter } from "react-router";
+
+const Edit = (props) => {
+  // This is the constructor that stores the data.
+  console.log("The constructor in edit is called")
+  const [person_name, setName] = useState("")
+  const [person_position, setPosition] = useState("")
+  const [person_level, setLevel] = useState("")
+  const [records, setRecords] = useState([])
+
+  // This will get the record based on the id from the database.
+  useEffect(() => {
+    console.log("USEFFECT CALLED")
+    axios
+      .get("http://localhost:3000/record/" + props.match.params.id)
+      .then((response) => (
+        setName(response.data.person_name),
+        setPosition(response.data.person_position),
+        setLevel(response.data.person_level))
+      ).catch(function (error) {
+        console.log(error);
+      })
+  }, [])
+
+  // These methods will update the state properties.
+  function onChangePersonName(eb) {
+    console.log(eb.target.value)
+    if (eb.target.value.length < 13) {
+      setName(eb.target.value)
+    }
+
+    if (eb.target.value.length === 12) {
+      console.log("you cannot add anymore")
+    }
+  }
+
+  function onChangePersonPosition(e) {
+    setPosition(e.target.value)
+
+  }
+
+  function onChangePersonLevel(e) {
+    setLevel(e.target.value)
+
+  }
+
+  // This function will handle the submission.
+  function onSubmit(e) {
+    e.preventDefault();
+    const newEditedperson = {
+
+      person_name: person_name,
+      person_position: person_position,
+      person_level: person_level,
+    };
+    console.log(newEditedperson);
+
+    // This will send a post request to update the data in the database.
+    axios
+      .post(
+        "http://localhost:3000/update/" + props.match.params.id,
+        newEditedperson
+      )
+      .then((res) => console.log(res.data));
+
+    props.history.push("/");
+  }
+
+  // This following section will display the update-form that takes the input from the user to update the data.
+  console.log("COOMPONENT RENDERED")
+
+  return (
+    <div>
+      <h3 align="center">Update Record</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Person's Name: </label>
+          <input
+            type="text"
+            className="form-control"
+            value={person_name}
+            onChange={onChangePersonName}
+          />
+        </div>
+        <div className="form-group">
+          <label>Position: </label>
+          <input
+            type="text"
+            className="form-control"
+            value={person_position}
+            onChange={onChangePersonPosition}
+          />
+        </div>
+        <div className="form-group">
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="priorityOptions"
+              id="priorityLow"
+              value="Intern"
+              checked={person_level === "Intern"}
+              onChange={onChangePersonLevel}
+            />
+            <label className="form-check-label">Intern</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="priorityOptions"
+              id="priorityMedium"
+              value="Junior"
+              checked={person_level === "Junior"}
+              onChange={onChangePersonLevel}
+            />
+            <label className="form-check-label">Junior</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="priorityOptions"
+              id="priorityHigh"
+              value="Senior"
+              checked={person_level === "Senior"}
+              onChange={onChangePersonLevel}
+            />
+            <label className="form-check-label">Senior</label>
+          </div>
+        </div>
+        <br />
+
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Update Record"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
+
+}
+
+// You can get access to the history object's properties and the closest <Route>'s match via the withRouter
+// higher-order component. This makes it easier for us to edit our records.
+
+export default withRouter(Edit);
