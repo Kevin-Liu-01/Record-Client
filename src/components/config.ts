5709a9bd05c41df13a37d84db36e8298f2b91c41
Kevin-Liu-01 <66856750+Kevin-Liu-01@users.noreply.github.com>
2021-08-23 10:58:56 -0400

    Changed server host

--- src/components/config.ts
+++ src/components/config.ts
@@ -1,7 +1,8 @@
 
 const Constants = {
-    // SERVER_HOST: "https://secret-thicket-69950.herokuapp.com"
-    SERVER_HOST: "http://localhost:3000"
+    // SERVER_HOST: "https://secret-thicket-69950.herokuapp.com"  //new js app
+    // SERVER_HOST: "http://localhost:3000"
+    SERVER_HOST: "https://github-record-client-server.herokuapp.com" //ts app
 
 }
 
